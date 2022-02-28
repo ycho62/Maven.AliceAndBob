07b704a093e7329b39bbdd80ea1d1e176e648587
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-02-27 22:30:55 -0500

    changes made

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +7,15 @@ public class Main {
 
     public static void main(String[] args ){
 
+        Scanner names = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        String name = names.nextLine();
+
+            if (name.equals("Alice") || name.equals("Bob")) {
+                System.out.println("Hello " + name);
+            } else {
+                System.out.println("Access denied");
+            }
+        }
     }
-}
+
